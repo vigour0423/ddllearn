dfa7936ec89908b070990d412ae1eab432490aca
dongdongliu <489582986@qq.com>
2018-10-14 00:46:51 +0800

    设计模式之建造者和原型模式

--- src/main/java/com/ddl/designpattern/prototype/Sheep.java
+++ src/main/java/com/ddl/designpattern/prototype/Sheep.java
@@ -4,45 +4,46 @@ import java.io.Serializable;
 import java.util.Date;
 
 
-public class Sheep implements Cloneable,Serializable {   //1997,英国的克隆羊，多利！
-	private String sname;
-	private Date birthday;
-	
-	
-	@Override
-	protected Object clone() throws CloneNotSupportedException {
-		Object obj = super.clone();  //直接调用object对象的clone()方法！
-		return obj;
-	}
+public class Sheep implements Cloneable, Serializable {   //1997,英国的克隆羊，多利！
+    private String sname;
+
+    private Date birthday;
 
 
-	public String getSname() {
-		return sname;
-	}
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        Object obj = super.clone();  //直接调用object对象的clone()方法！
+        return obj;
+    }
 
 
-	public void setSname(String sname) {
-		this.sname = sname;
-	}
+    public String getSname() {
+        return sname;
+    }
 
 
-	public Date getBirthday() {
-		return birthday;
-	}
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
 
 
-	public void setBirthday(Date birthday) {
-		this.birthday = birthday;
-	}
+    public Date getBirthday() {
+        return birthday;
+    }
 
 
-	public Sheep(String sname, Date birthday) {
-		super();
-		this.sname = sname;
-		this.birthday = birthday;
-	}
-	
-	public Sheep() {
-	}
-	
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+
+    public Sheep(String sname, Date birthday) {
+        super();
+        this.sname = sname;
+        this.birthday = birthday;
+    }
+
+    public Sheep() {
+    }
+
 }
