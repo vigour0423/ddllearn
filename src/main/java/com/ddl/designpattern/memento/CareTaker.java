bc2c6a4330884aab0cd625a79bf8e855dff1073f
dongdongliu <489582986@qq.com>
2018-10-20 10:32:12 +0800

    设计模式之备忘录模式

--- src/main/java/com/ddl/designpattern/memento/CareTaker.java
+++ src/main/java/com/ddl/designpattern/memento/CareTaker.java
@@ -4,25 +4,21 @@ package com.ddl.designpattern.memento;
  * 负责人类
  * 负责管理备忘录对象
  * @author Administrator
- *
  */
 public class CareTaker {
-	
-	private EmpMemento memento;
 
-//	private List<EmpMemento> list = new ArrayList<EmpMemento>();
-	
-	
-	
-	public EmpMemento getMemento() {
-		return memento;
-	}
+    private EmpMemento memento;
 
-	public void setMemento(EmpMemento memento) {
-		this.memento = memento;
-	}
-	
-	
-	
-	
+    //	private List<EmpMemento> list = new ArrayList<EmpMemento>();
+
+
+    public EmpMemento getMemento() {
+        return memento;
+    }
+
+    public void setMemento(EmpMemento memento) {
+        this.memento = memento;
+    }
+
+
 }
